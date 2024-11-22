c38c30807b9a4fc76f3d88b4b5cc98195a19e9a2
e8olution <162801527+e8olution@users.noreply.github.com>
2024-11-22 21:44:26 +0300

    2 migration

--- main/models.py
+++ main/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+    manufactured_at = models.DateField(verbose_name='Дата производства продукта', null=True, blank=True)
 
     def __str__(self):
         return self.name
